fa439999d4f35094b074d6070a1cb6bd41d5e392
wuchen <wuchen@wuchens-MacBook-Air.local>
2016-10-02 13:19:15 -0700

    Hint for 2-2-4

---
+++ hw3 /hint for 2-2-4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a;
+	cin>>a;
+	if (a=="+") {
+		cout<<3+5<<endl;
+	} else {
+	    cout<<3-5<<endl;
+	}
+
+	return 0;
+}
